428c3c4642926b29751c79b9b501e14eae1e1da8
Ivar Wohlfeil <woiv1011@hs-karlsruhe.de>
2017-05-09 06:22:28 +0200

    buffer.c update

--- buffer.c
+++ buffer.c
@@ -9,3 +9,22 @@ getCurrentLine() {
 getCurrentColumn() {
 
 }
+
+char getNextChar() {
+  char temp = 0;
+  static int lineCount = 1;
+  static int columnCount = 1;
+
+  temp = currentChar;
+  position++;
+
+  return temp;
+}
+
+goToNext()
+
+next()      //move 1 char to the right
+next(int i) //move i chars to the right
+
+previous() //move 1 char to the left
+previous(int i) //move i chars to the left
