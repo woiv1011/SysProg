feccdacd56a8f17379f5d6386c93cad573439200
Ivar Wohlfeil <woiv1011@hs-karlsruhe.de>
2017-05-15 08:12:59 +0200

    added tokenList  + print

--- tokenize.h
+++ tokenize.h
@@ -15,14 +15,67 @@ typedef token_t struct {
 }
 
 typedef tokennode_t struct {
-  //long         index;
   tokennode_t *next;
-  tokennode_t *previous;
   token_t      data;
 }
 
+static tokennode_t *startNode;
 
+appendTokenToList(token_t tok) {
+  static int tokenListSize = 0;
+  static tokennode_t *currentNode;
+  static tokennode_t *newNode;
 
+  newNode = malloc(sizeof(tokennode_t));
+  newNode->next = NULL;
+  newNode->data = tok;
+
+  if(tokenListSize == 0) {
+    startNode = newNode; //startnnde wird ab hier nicht mehr verändert
+    currentNode = newNode; //am anfang current = start
+  }
+
+  else { //tokenListSize >= 1
+    currentNode->next = newNode;
+    currentNode = newNode;
+  }
+
+  tokenListSize++;
+}
+
+tokennode_t getCurrentNode() {
+
+}
+
+void printTokenList() {
+  static tokennode_t *currentNode;
+  static int index = 0;
+  int i = 0;
+  if(index == 0) {
+    currentNode = startNode;
+  }
+
+  while(1) {
+    printf("i: %8d ", index);
+    printf(" line: %3d", currentNode->data.line);
+    printf(" col: %3d", currentNode->data.column);
+    printf(" length: %5d", currentNode->data.length);
+    printf(" value: "); //TODO stattdessen mit %s ? -> benötigt stringende zeichen
+    for(i=0; i<currentNode->data.length; i++) {
+      printf("%c", currentNode->data.value[i]);
+    }
+    printf("\n");
+
+    if(currentNode->next == NULL) {
+      break;
+    }
+    else {
+      currentNode = currentNode->next;
+    }
+  }
+  
+
+}
 
 token_t createToken(tokentype_t type, unsigned short length, int line, int column, char *value) {
   token_t result;
@@ -31,15 +84,10 @@ token_t createToken(tokentype_t type, unsigned short length, int line, int colum
   result.line = line;
   result.column = column;
 
-  result.value = malloc(len);
+  result.value = malloc(len+1);
   strncpy(result.value, value, len);
 
   return result;
-}
-
-appendTokenToList(token_t tok) {
-  static int tokenListSize = 0;
-
 }
 
 #endif
